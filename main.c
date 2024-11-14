a3f091375f4637ce58a4e9ac9c40c54fc0ebf44f
Denver2002620 <abelg@myuax.com>
2024-11-14 12:59:37 +0100

    add

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
-#include <sys/mman.h>
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <windows.h>
 
 #define SIZE 4096
 
+// Define necessary constants
+#define PROT_READ 0x1
+#define PROT_WRITE 0x2
+#define MAP_SHARED 0x01
+#define MAP_ANONYMOUS 0x20
+#define MAP_FAILED ((void *)-1)
+
+// Declare mmap function prototype
+void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+int munmap(void *addr, size_t length);
+
 int main() {
-    char *shared_memory = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    char *shared_memory = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if (shared_memory == MAP_FAILED) {
         perror("mmap");
         exit(EXIT_FAILURE);
@@ -25,9 +37,8 @@ int main() {
         exit(EXIT_SUCCESS);
     } else {
         strcpy(shared_memory, "Hello, child process!");
-        wait(NULL);
+        waitpid(pid, NULL, 0);
         munmap(shared_memory, SIZE);
     }
 
     return 0;
-}
